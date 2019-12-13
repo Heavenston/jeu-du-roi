27796ba60fb3558c5e6a115effbf546d8149af79
Heavenston <malolegendrelemaire@gmail.com>
2019-12-13 19:10:22 +0000

    fixed firebase reinitialization

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -1,12 +1,13 @@
 import firebase from "firebase/app"
-/*
-firebase.initializeApp({
-  apiKey: "AIzaSyAlQy4TfIFJYCi9ynFlj4WhLeKPBHGR5i4",
-  authDomain: "kinggame-d06b7.firebaseapp.com",
-  databaseURL: "https://kinggame-d06b7.firebaseio.com",
-  projectId: "kinggame-d06b7",
-  storageBucket: "kinggame-d06b7.appspot.com",
-  messagingSenderId: "313270485734",
-  appId: "1:313270485734:web:6321050c987529f1167127",
-  measurementId: "G-GJE25WNTYT"
-});*/
+
+if (!firebase.app)
+  firebase.initializeApp({
+    apiKey: "AIzaSyAlQy4TfIFJYCi9ynFlj4WhLeKPBHGR5i4",
+    authDomain: "kinggame-d06b7.firebaseapp.com",
+    databaseURL: "https://kinggame-d06b7.firebaseio.com",
+    projectId: "kinggame-d06b7",
+    storageBucket: "kinggame-d06b7.appspot.com",
+    messagingSenderId: "313270485734",
+    appId: "1:313270485734:web:6321050c987529f1167127",
+    measurementId: "G-GJE25WNTYT"
+  });
