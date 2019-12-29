f85a0210d526d474b7d8ea0272828e14f17e0c63
Heavenston <malolegendrelemaire@gmail.com>
2019-12-29 10:42:27 +0100

    Adde range icon

---
+++ components/RangeIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-icon>
+    {{current}}
+  </v-icon>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: "RangeIcon",
+
+  props: {
+    items: {
+      type: Array as () => string[],
+      required: true,
+    },
+    value: {
+      type: Number,
+      default: 0
+    }
+  },
+
+  computed: {
+    current(): string {
+      return this.items[this.value];
+    }
+  }
+})
+</script>
