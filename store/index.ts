12efa5cd39abd0d3895070899a0ba51a9c55deec
Heavenston <malolegendrelemaire@gmail.com>
2019-12-29 10:55:55 +0100

    Emojies for fifficulties

---
+++ store/index.ts
@@ -0,0 +1,20 @@
+import { GetterTree, ActionTree, MutationTree } from 'vuex'
+
+export const state = () => ({
+  difficultyEmojies: [{
+    text: "mdi-emoticon-neutral-outline",
+    props: {color: "blue lighten-4"}
+  },
+  {
+    text: "mdi-emoticon-outline",
+    props: {color: "orange darken-1"}
+  },
+  {
+    text: "mdi-emoticon-devil-outline",
+    props: {color: "#CE93D8"}
+  },
+  {
+    text: "mdi-fire",
+    props: {color: "red darken-2"}
+  }]
+});
